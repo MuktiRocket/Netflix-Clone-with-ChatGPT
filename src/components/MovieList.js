332b29b820bdb747c0f6a61cce253f72da8b60b3
Jaydeep Sarkar <jaydeeps96s@gmail.com>
2023-09-25 11:37:34 +0530

    minor change

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -5,7 +5,7 @@ const MovieList = ({ title, movies }) => {
   return (
     <div className="md:px-6">
       <h1 className="md:text-3xl text-xl py-2 text-white">{title}</h1>
-      <div className="md:container-snap flex overflow-x-scroll">
+      <div className="container-snap flex overflow-x-scroll">
         <div className="flex">
           {movies?.map((movie) => (
             <MovieCard key={movie.id} posterPath={movie.poster_path} />
