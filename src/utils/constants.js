b7e2487abf2a83f9d22c38a3c2f94227be70f8da
Jaydeep Sarkar <jaydeeps96s@gmail.com>
2023-09-29 23:42:13 +0530

    deploying

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,7 +7,7 @@ export const API_OPTIONS = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMjQ2NzhhOGM0OWFjMWI1YzQwNWRjNDI3OWMxMDQ0YyIsInN1YiI6IjY1MGVkM2Q3ZThkMGI0MDEyZDVjZmIyMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.JCISiIcYEqRf26Wm0FSUkA8y0XOcZASguIbeaxBAn-0",
+    Authorization: "Bearer " + process.env.REACT_APP_TMDB_KEY,
   },
 };
 export const IMG_CDN = "https://image.tmdb.org/t/p/w500";
